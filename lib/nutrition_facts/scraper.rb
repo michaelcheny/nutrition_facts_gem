f8265120ef5123cee12fbec6c4aa236d1e52b3ae
Michael Chen <michaelchengaming@gmail.com>
2019-06-05 18:22:06 -0700

    pagination working, scraping over 290 pages

--- lib/nutrition_facts/scraper.rb
+++ lib/nutrition_facts/scraper.rb
@@ -4,7 +4,7 @@ class FoodScraper
 
   # STATIC_URL = "https://www.nutritionix.com/database/common-foods"
     
-  STATIC_URL = "https://www.nutritionix.com/nixapi/search/en_US"
+  # STATIC_URL = "https://www.nutritionix.com/nixapi/search/en_US"
 
 
   # def get_page
@@ -14,23 +14,36 @@ class FoodScraper
   
   ## scrape the first level of the website for common food name, calories, and serving size
   def self.scrape_foods
-    food_items = []
-    parsed_page = HTTParty.get(STATIC_URL)
+    food_database_url = "https://www.nutritionix.com/nixapi/search/en_US"
+    parsed_page = HTTParty.get(food_database_url)
 
+    food_items = []
     page = 1
     items_per_page = parsed_page["foods"].count
-    total_items = parsed_page["total"].to_f
-    last_page = (total_items.to_f / items_per_page.to_f).round
+    total_items = parsed_page["total"]
+    last_page = (total_items.to_f / items_per_page.to_f).ceil   # because there can be only a few items in lastpage
+    # binding.pry
 
-    binding.pry
+    while page <= last_page
+      pagination_url = "https://www.nutritionix.com/nixapi/search/en_US?page=#{page}"
 
-    parsed_page["foods"].map do |food|
-      food_items << {
-      :name => food["food_name"],
-      :calories_per_serving => food["nf_calories"],
-      :grams_per_serving => food["serving_weight_grams"]
-      }
+      puts pagination_url
+      puts "Page: #{page}"
+
+      pagination_parsed_page = HTTParty.get(pagination_url)
+
+      pagination_parsed_page["foods"].map do |food|
+        food_items << {
+        :name => food["food_name"],
+        :calories_per_serving => food["nf_calories"],
+        :grams_per_serving => food["serving_weight_grams"]
+        }
+        puts "Added #{food["food_name"]}"
+        puts ""
+      end
+      page += 1
     end
+    # binding.pry
     food_items
   end
 
